e844ad39c43d1abb413aed46a99fd5592794e5ad
pablo <pablo@host.docker.internal>
2024-04-04 16:48:58 +0200

    Fix template module modevo-script test

--- modevo-script/src/main/java/giis/m2/Class2.java
+++ modevo-script/src/main/java/giis/m2/Class2.java
@@ -7,7 +7,6 @@ public class Class2 {
 	private static final Logger log=LoggerFactory.getLogger(Class2.class);
 	public String function21() {
 		log.info("Run function21()");
-		Class2 class2 = new Class2();
-		return class2.function21() + "-" + class2.function21();
+		return "11-12";
 	}
 }
