a95d9233cbb2fc66e752c6aae70d85aa33969f6f
suarezgpablo <suarezgpablo@uniovi.es>
2024-04-24 12:10:12 -0400

    Review container setup (#25)
    * Sleepers replaced
    
    * Fixed to ensure that superuser is created
    
    * Override wait time for superuser setup delay to 0
    
    * Removed override of credentials requirements for GitHub actions
    
    container
    
    * Fix mistake test.yml
    
    * Removed unused properties and relocated and renamed
    
    ---------
    
    Co-authored-by: pablo <pablo@host.docker.internal>

--- setup/wait-container-ready.sh
+++ setup/wait-container-ready.sh
@@ -2,17 +2,20 @@
 #Sample approach to wait until a container is ready
 #by looking for a string in container log
 container=$1
-target=$2
+countertarget=$2
+target=$3
 
 attempt=0
 while [ $attempt -le 60 ]; do
     attempt=$(( $attempt + 1 ))
     echo "Waiting for container ready (attempt: $attempt)..."
     result=$(docker logs $container)
-    if grep -q "$target" <<< $result ; then
-      echo "Container is ready!"
-      exit 0
+    count=$(grep -o "$target" <<< "$result" | wc -l)
+    if [ $count -eq $countertarget ]; then
+        echo "Container is ready!"
+        exit 0
     fi
+    
     sleep 1
 done
 echo "ERROR: Container is not ready after maximum number of attempts"
