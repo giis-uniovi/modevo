f122c675994fcaf6b2a1b27b402eec36ff1a758c
suarezgpablo <suarezgpablo@uniovi.es>
2024-07-02 04:02:07 -0400

    Fix new Sonarcloud issues that apeared in main branch (#55)
    * Removed issues sonarqube
    
    * Changed name constants to follow snakecase convention

--- modevo-transform/src/test/java/test4giis/modevo/TestTransform.java
+++ modevo-transform/src/test/java/test4giis/modevo/TestTransform.java
@@ -12,10 +12,10 @@ import org.junit.rules.TestName;
 
 public class TestTransform {
 	
-	private static final String inputModelsFolderTemp = "target/input-models/";
-	private static final String schema = "schema.xmi";
-	private static final String cm = "CM.xmi";
-	private static final String schemaChange = "schemaChange.xmi";
+	private static final String INPUT_MODELS_FOLDER_TEMP = "target/input-models/";
+	private static final String SCHEMA = "schema.xmi";
+	private static final String CM = "CM.xmi";
+	private static final String SCHEMA_CHANGE = "schemaChange.xmi";
 	 
 	@Rule public TestName name = new TestName();
 	
@@ -24,10 +24,10 @@ public class TestTransform {
 	 */
 	@Before
 	public void removeTempModelFolder() throws IOException  {
-		Path schemaTempPath = Paths.get(inputModelsFolderTemp+schema);
-		Path cmTempPath = Paths.get(inputModelsFolderTemp+cm);
-		Path schemaChangeTempPath = Paths.get(inputModelsFolderTemp+schemaChange);
-		Path tempFolder = Paths.get(inputModelsFolderTemp);
+		Path schemaTempPath = Paths.get(INPUT_MODELS_FOLDER_TEMP+SCHEMA);
+		Path cmTempPath = Paths.get(INPUT_MODELS_FOLDER_TEMP+CM);
+		Path schemaChangeTempPath = Paths.get(INPUT_MODELS_FOLDER_TEMP+SCHEMA_CHANGE);
+		Path tempFolder = Paths.get(INPUT_MODELS_FOLDER_TEMP);
 		Files.deleteIfExists (schemaTempPath);
 		Files.deleteIfExists (cmTempPath);
 		Files.deleteIfExists (schemaChangeTempPath);
